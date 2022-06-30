527bf02a13edc478e7b959050e4d209bc6a730e3
abarne <celius377@gmail.com>
2022-06-30 16:11:59 -0400

    tab controller added

---
+++ PickAndPay/HomePageViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HomePageViewController.swift
+//  PickAndPay
+//
+//  Created by admin on 6/30/22.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+
+    let data = [dummyData(name: "Test1"), dummyData(name: "Test2")]
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //pageControl.numberOfPages = data.count
+        
+        
+    }
+    
+
+   
+
+}
+
+extension HomePageViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath.row) as! HomePageCategoryCollectionViewCell
+    }
+    
+    
+}
+
+struct dummyData {
+    var name : String
+}
+
