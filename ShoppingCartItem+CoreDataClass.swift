b38a6ab610a8f9bea6a41ba294d07b2aa224bad7
abarne <celius377@gmail.com>
2022-07-07 10:46:36 -0400

    Fix Core Data compile issues

---
+++ ShoppingCartItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingCartItem+CoreDataClass.swift
+//  PickAndPay
+//
+//  Created by admin on 7/7/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ShoppingCartItem)
+public class ShoppingCartItem: NSManagedObject {
+
+}
